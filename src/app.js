4db8ee7bb39e3055d801c0f38751c01a2eb30631
nluong0 <64280847+nluong0@users.noreply.github.com>
2021-03-19 02:34:40 -0500

    Filtering by dropdown for sectors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ function myVis(data, us){
   console.log('data', data);
   console.log('us', us);
 
-  // Set constants
+  // Set aesthetic constants
   var margin = {top: 0, right: 0, bottom: 0, left: 0}
   let width = 960 - margin.left - margin.right
   let height = 600 - margin.top - margin.bottom;
@@ -27,21 +27,20 @@ function myVis(data, us){
   let path = d3.geoPath().projection(projection)
 
   // Set colors
+  let uniqueSectors = ["Farming", "Government", "Manufacturing", "Mining", "Nonspecialized", "Recreation"];
   const color = d3.scaleOrdinal()
-    .domain(["Farming", "Government", "Manufacturing", "Mining", "Nonspecialized", "Recreation"])
+    .domain(uniqueSectors)
     .range(['#A89938', '#E49B25', '#74AA90', '#3D405B', '#E6DFB3', '#E07A5F']);
 
   // Define SVG elements
   const svgContainer = select('#map')
     .append('div')
     .style('position', 'relative');
-  
   const svg = svgContainer
     .append('svg')
     .attr('height', height)
     .attr('width', width)
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
-  
   const tooltip = svgContainer
     .append('div')
     .attr('id', 'tooltip');
@@ -59,8 +58,35 @@ function myVis(data, us){
     return matchedRow;
   }
 
-  // Add counties and populate map
-  svg
+  // Add options to dropdown
+  d3.select("#selectButton")
+    .selectAll('myOptions')
+    .data(['All'].concat(uniqueSectors))
+    .enter()
+    .append('option')
+    .text(function (d) { return d; })
+    .attr("value", function (d) { return d; })
+
+  // Manage updates with dropdown selections
+  d3.select('#selectButton')
+    .on("change", function(d) {
+      // Grab selected option
+      var selectedSector = d3.select(this).property("value")
+      // Filter data according to selected option
+      var dataFilter = data.filter(function(d){return d.all_sector_dependencies==selectedSector})
+      if (selectedSector === 'All') {
+        dataFilter = data
+      }
+      // Render plot with filtered data
+      renderPlot(dataFilter)
+  });
+
+  renderPlot(data);
+
+  function renderPlot(data) {
+
+    // Add counties and populate map
+    svg
     .append("g")
     .attr("class", "county")
     .selectAll("path")
@@ -69,114 +95,115 @@ function myVis(data, us){
     .attr("d", path)
     .style("fill", function(d) { 
       let matchedRow = findSectorData(data, d.id)
-      if (typeof matchedRow !== 'undefined') {
+      if (typeof matchedRow !== "undefined") {
           return color(matchedRow.all_sector_dependencies)
       }
-    ;})
+      else {
+        return "#E0E0E0"
+      }
+    ;}
+    )
 
-  // Setting stroke
-  // Counties
-  svg
-    .selectAll(".county")
-    .attr("stroke", "none");
-  // States
-  // svg
-  //   .select("g")
-  //   .append("path")
-  //   .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-  //   .attr("fill", "none")
-  //   .attr("stroke", "black")
-  //   .attr("d", path);
+    // Setting stroke
+    // Counties
+    svg
+      .selectAll(".county")
+      .attr("stroke", "none");
+    // States
+    // svg
+    //   .select("g")
+    //   .append("path")
+    //   .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+    //   .attr("fill", "none")
+    //   .attr("stroke", "black")
+    //   .attr("d", path);
 
-  // Format tooltip
-  function callout(g, value) {
-    if (!value) return g.style("display", "none");
-
-    g.style("display", null)
-        .style("pointer-events", "none")
-        .style("font", "10px sans-serif");
-
-    var path = g
-        .selectAll("path")
-        .data([null])
-        .join("path")
-        .attr("fill", "white")
-        .attr("stroke", "black");
-
-    var text = g
-        .selectAll("text")
-        .data([null])
-        .join("text")
-        .call(function(text) {
-            text.selectAll("tspan")
-                .data((value + "").split("/\n/"))
-                .join("tspan")
-                .attr("x", 0)
-                .attr("y", function(d, i) {
-                    return i * 1.1 + "em";
-                })
-                .style("font-weight", function(_, i) {
-                    return i ? null : "bold";
-                })
-                .text(function(d) {
-                    return d;
-                });
-        });
-  }
-
-  // Create tooltip
-  svg
-    .selectAll(".county")
-    .on("mouseover", function(d) {
-      console.log('td.target', d.target)
-      console.log('d.target.__data__.id', d.target.__data__.id)
-      //let countyName = d.target.__data__.properties.name;
-      let matchedRow = findSectorData(data, d.target.__data__.id);
-      var displayText = displayText = 'No Data';
-
-      if (matchedRow) {
-        var countyName = matchedRow.County;
-        var stateName = matchedRow.State;
-        displayText = countyName + ', ' + stateName;
-      };
-
-      tooltip.call(
-        callout,
-        countyName + ", " + stateName
-      )
-      .style('left', `${d.offsetX}px`)
-      .style('top', `${d.offsetY}px`);
-      
-      d3.select(d.target)
-        .attr("stroke", "black")
-        .raise();
-
-    })
-    .on('mouseout', function(d) {
-      tooltip.call(callout, null)
-
-      d3.select(d.target)
-        .attr("stroke", "none")
-        .lower();
-    });
-
-  // Create legend
-  var legend = svg.selectAll('g.legendEntry')
-      .data(color.range())
-      .enter()
-      .append('g').attr('class', 'legendEntry')
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    // Format tooltip
+    function callout(g, value) {
+      if (!value) return g.style("display", "none");
   
-  legend
-      .append('rect')
-      .attr("x", width - 120)
-      .attr("y", height - 200)
-      .attr("width", 10)
-      .attr("height", 10)
-      .style("stroke", "black")
-      .style("stroke-width", 1)
-      .style("fill", function(d){return d;}); 
-
+      g.style("display", null)
+          .style("pointer-events", "none")
+          .style("font", "10px sans-serif");
+  
+      var path = g
+          .selectAll("path")
+          .data([null])
+          .join("path")
+          .attr("fill", "white")
+          .attr("stroke", "black");
+  
+      var text = g
+          .selectAll("text")
+          .data([null])
+          .join("text")
+          .call(function(text) {
+              text.selectAll("tspan")
+                  .data((value + "").split("/\n/"))
+                  .join("tspan")
+                  .attr("x", 0)
+                  .attr("y", function(d, i) {
+                      return i * 1.1 + "em";
+                  })
+                  .style("font-weight", function(_, i) {
+                      return i ? null : "bold";
+                  })
+                  .text(function(d) {
+                      return d;
+                  });
+          });
+    }
+  
+    // Create tooltip
+    svg
+      .selectAll(".county")
+      .on("mouseover", function(d) {
+  
+        let matchedRow = findSectorData(data, d.target.__data__.id);
+        var displayText = 'No Data';
+  
+        if (matchedRow) {
+          var countyName = matchedRow.County;
+          var stateName = matchedRow.State;
+          displayText = countyName + ', ' + stateName;
+        };
+  
+        tooltip.call(
+          callout,
+          displayText
+        )
+        .style('left', `${d.offsetX}px`)
+        .style('top', `${d.offsetY}px`);
+        
+        d3.select(d.target)
+          .attr("stroke", "black")
+          .raise();
+  
+      })
+      .on('mouseout', function(d) {
+        tooltip.call(callout, null)
+  
+        d3.select(d.target)
+          .attr("stroke", "none")
+          .lower();
+      });
+  
+    // Create legend
+    var legend = svg.selectAll('g.legendEntry')
+        .data(color.range())
+        .enter()
+        .append('g').attr('class', 'legendEntry')
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    
+    legend
+        .append('rect')
+        .attr("x", width - 120)
+        .attr("y", height - 200)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("stroke", "black")
+        .style("stroke-width", 1)
+        .style("fill", function(d){return d;}); 
+  }
 }
 
-// add mouseleave to entire svg not just tooltip
